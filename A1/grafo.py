3461a8d5833d7c2866aa5908ba3c7fb82f0e8536
unknown <luan.diniz.moraes@hotmail.com>
2023-03-19 17:24:26 -0300

    Tentativa de implementacao 1

---
+++ A1/grafo.py
@@ -0,0 +1,37 @@
+import aresta
+
+class Grafo:
+    def __init__(self):
+        self.__vertices = None
+        self.__arestas = None
+        #self.__peso = None
+        
+    
+    def qtdVertices():
+        pass
+
+    def qtdArestas():
+        pass
+    
+    def grau():
+        pass
+
+    def rotulo():
+        pass
+
+    def vizinhos():
+        pass
+
+    def haAresta():
+        pass
+
+    def peso():
+        pass
+
+    def lerArquivo():
+        pass
+
+
+grafeto = Grafo()
+print(grafeto)
+        
