cb9be19b1e361dce7b28cc34fc9f42f1c4463cc6
LucasTruppel <lucasctruppelm@gmail.com>
2023-04-10 13:24:10 -0300

    Classe de teste de grafo concluida

--- A1/main.py
+++ A1/main.py
@@ -4,10 +4,4 @@ from test_grafo import TesteGrafo
 grafeto = Grafo("grafo.txt")
 teste_grafo = TesteGrafo(grafeto)
 
-teste_grafo.matriz()
-print()
-teste_grafo.qtdVertices()
-print()
-teste_grafo.qtdArestas()
-print()
-teste_grafo.grau()
+teste_grafo.realizar_todos_testes()
--- A1/test_grafo.py
+++ A1/test_grafo.py
@@ -20,3 +20,40 @@ class TesteGrafo:
         print("Teste de grau()")
         for i in range(self.grafo.qtdVertices()):
             print(f"Vertice {i+1}: {self.grafo.grau(i+1)}")
+    
+    def rotulo(self) -> None:
+        print("Teste de rotulo()")
+        for i in range(self.grafo.qtdVertices()):
+            print(f"Rotulo de {i+1}: {self.grafo.rotulo(i+1)}")
+    
+    def vizinhos(self) -> None:
+        print("Teste de vizinhos()")
+        for i in range(self.grafo.qtdVertices()):
+            print(f"Vizinhos {i+1}: {self.grafo.vizinhos(i+1)}")
+    
+    def haAresta(self) -> None:
+        print("Teste de haAresta()")
+        for i in range(self.grafo.qtdVertices()):
+            for j in range(self.grafo.qtdVertices()):
+                print(f"Aresta {i+1}-{j+1}: {self.grafo.haAresta(i+1, j+1)}")
+    
+    def peso(self) -> None:
+        print("Teste de peso()")
+        for i in range(self.grafo.qtdVertices()):
+            for j in range(self.grafo.qtdVertices()):
+                print(f"Peso {i+1}-{j+1}: {self.grafo.peso(i+1, j+1)}")
+            
+    def realizar_todos_testes(self):
+        self.matriz()
+        print()
+        self.qtdVertices()
+        print()
+        self.qtdArestas()
+        print()
+        self.grau()
+        print()
+        self.rotulo()
+        print()
+        self.haAresta()
+        print()
+        self.peso()
