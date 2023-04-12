8f89714bc00c8ffb08c15761e3e5cba0b8616d35
unknown <luan.diniz.moraes@hotmail.com>
2023-04-12 18:18:41 -0300

    Adicionado teste Bellman-Ford no main.py

--- A1/main.py
+++ A1/main.py
@@ -1,6 +1,7 @@
 from grafo import Grafo
 from test_grafo import TesteGrafo
 from busca import Busca
+from caminho_minimo import CaminhoMinimo
 
 print("Questão 1, Representação:\n")
 grafo = Grafo("grafo.txt")
@@ -9,3 +10,9 @@ teste_grafo.realizar_todos_testes()
 
 print("\n\nQuestão 2, Buscas:\n")
 Busca.buscar("grafo.txt", 1)
+
+
+
+print("\n\nQuestão 4, Bellman-Ford:\n")
+CaminhoMinimo.buscar("grafo.txt", 1)
+
