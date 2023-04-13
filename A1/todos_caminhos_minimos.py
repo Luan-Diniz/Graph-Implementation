334ed767da0e4999010fae7408784e1fa6368ee1
unknown <luan.diniz.moraes@hotmail.com>
2023-04-12 21:15:17 -0300

    Inicio do FloydMarshall

---
+++ A1/todos_caminhos_minimos.py
@@ -0,0 +1,25 @@
+from grafo import Grafo
+
+class TodosCaminhosMinimos:
+    @staticmethod
+    def buscar(nome_arquivo: str):
+         grafo = Grafo(nome_arquivo)
+         #usar FloydWarshall e printar certo
+        
+
+    @staticmethod
+    def FloydWarshall(grafo: Grafo) -> list:
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    
+    quantidade_args = len(sys.argv)
+    if quantidade_args != 3:
+        print("2 argumentos necessários: nome_arquivo e indice_vertice")
+    else:
+        CaminhoMinimo.buscar(sys.argv[1], int(sys.argv[2]))
