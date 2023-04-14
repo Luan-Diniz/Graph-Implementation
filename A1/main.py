52b18195209a36a8e9d65fda008db4247dede958
unknown <alexdavisneuwiem@gmail.com>
2023-04-14 17:05:01 -0300

    Alterando o main.py

--- A1/main.py
+++ A1/main.py
@@ -1,6 +1,7 @@
 from grafo import Grafo
 from test_grafo import TesteGrafo
 from busca import Busca
+from hierholzer import Hierholzer
 from caminho_minimo import CaminhoMinimo
 
 print("Questão 1, Representação:\n")
@@ -11,7 +12,9 @@ teste_grafo.realizar_todos_testes()
 print("\n\nQuestão 2, Buscas:\n")
 Busca.buscar("grafo.txt", 1)
 
-
+print("\n\nQuestão 3, Hierholzer:\n")
+grafo = Grafo("grafo.txt")
+Hierholzer.encontrar_ciclo_euleriano(grafo)
 
 print("\n\nQuestão 4, Bellman-Ford:\n")
 CaminhoMinimo.buscar("grafo.txt", 1)
