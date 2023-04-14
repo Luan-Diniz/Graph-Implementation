8bba2af6f78c380f9aefdaebe2a168c0dbe2d7d3
unknown <luan.diniz.moraes@hotmail.com>
2023-04-14 17:11:51 -0300

    Floyd Warshall Funcionando e mais um grafo teste.

--- A1/todos_caminhos_minimos.py
+++ A1/todos_caminhos_minimos.py
@@ -4,12 +4,38 @@ class TodosCaminhosMinimos:
     @staticmethod
     def buscar(nome_arquivo: str):
          grafo = Grafo(nome_arquivo)
-         #usar FloydWarshall e printar certo
+         matriz_resultado = TodosCaminhosMinimos.FloydWarshall(grafo)
+
+         quantidade_vertices = grafo.qtdVertices()
+         for i in range(0, quantidade_vertices):
+             print(f"{i+1}:", end = '')
+             for j in range(0, quantidade_vertices):
+                if j == quantidade_vertices - 1:
+                    print(f"{matriz_resultado[i][j]}", end = "")
+                else:
+                    print(f"{matriz_resultado[i][j]},", end = "")
+             print()
         
 
     @staticmethod
     def FloydWarshall(grafo: Grafo) -> list:
-        pass
+        number_of_vertices = grafo.qtdVertices()
+
+        distance = [([float('inf') for j in range(number_of_vertices)]) for i in range(number_of_vertices)]   #Cria a matriz de distancias
+        for i in range(0, number_of_vertices):
+            distance[i][i] = 0
+        for u,v in grafo.getArestas():
+            distance[u-1][v-1] = grafo.peso(u,v)
+        
+        for k in range(1, number_of_vertices):
+            for j in range(1, number_of_vertices):
+                for i in range(1, number_of_vertices):
+                    if distance[i-1][j-1] > distance[i-1][k-1] + distance[k-1][j-1]:
+                        distance[i-1][j-1] = distance[i-1][k-1] + distance[k-1][j-1]
+        
+        return distance
+
+
 
 
 
@@ -19,7 +45,7 @@ if __name__ == "__main__":
     import sys
     
     quantidade_args = len(sys.argv)
-    if quantidade_args != 3:
-        print("2 argumentos necessários: nome_arquivo e indice_vertice")
+    if quantidade_args != 2:
+        print("1 argumentos necessários: nome_arquivo")
     else:
-        TodosCaminhosMinimos.buscar(sys.argv[1], int(sys.argv[2]))
+        TodosCaminhosMinimos.buscar(sys.argv[1])
